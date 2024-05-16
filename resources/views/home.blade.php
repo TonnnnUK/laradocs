1c1cc25384c62878adeccfffea13677ac91dc33d
Anton Hutchinson <a.hutchinson86@gmail.com>
2024-05-16 17:44:06 +0100

    Logo links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,49 +58,49 @@
             <!-- Logos -->
             <div class="mt-4 opacity-60 w-full flex flex-wrap">
                 <div class="w-full flex flex-wrap justify-center mb-4 gap-y-2 md:gap-y-6">
-                    <div class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center gap-2">
+                    <a href="https://laravel.com/docs/11.x/" class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center gap-2">
                         <img width="20" height="20" class="inline w-8" src="/img/icons/laravel.svg" />  
                         <img width="120" height="30" class="inline md:w-28" src="/img/logos/laravel-text.svg" />  
-                    </div>
-                    <div class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
+                    </a>
+                    <a href="https://livewire.laravel.com/docs/quickstart" class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
                         <img width="120" height="30" class="inline md:w-40" src="/img/logos/livewire.svg" />  
-                    </div>
-                    <div class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
+                    </a>
+                    <a href="https://tailwindcss.com/docs/installation" class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
                         <x-tailwind-icon class="w-[80%] md:w-40" />
-                    </div>
-                    <div class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
+                    </a>
+                    <a href="https://alpinejs.dev/start-here" class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
                         <img width="120" height="30" class="inline md:w-36" src="/img/logos/alpine.svg" />  
-                    </div>
-                    <div class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
+                    </a>
+                    <a href="https://inertiajs.com/" class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
                         <x-inertia-icon class="md:w-36 bg-purple-500 px-1 pt-1 pb-2" />
-                    </div>
-                    <div class="w-1/2 md:w-1/4 mb-4 flex justify-center items-center">
+                    </a>
+                    <a href="https://jetstream.laravel.com/introduction.html" class="w-1/2 md:w-1/4 mb-4 flex justify-center items-center">
                         <x-jetstream-icon class="w-[80%] md:w-40" />
-                    </div>
-                    <div class="w-1/2 md:w-1/4 mb-4 flex justify-center items-center">
+                    </a>
+                    <a href="https://vuejs.org/guide/introduction.html" class="w-1/2 md:w-1/4 mb-4 flex justify-center items-center">
                         <img width="120" height="30" class="inline md:w-32" src="/img/logos/vue.png" />  
-                    </div>
-                    <div class="w-1/2 md:w-1/4 mb-4 flex justify-center items-center">
+                    </a>
+                    <a href="https://react.dev/learn" class="w-1/2 md:w-1/4 mb-4 flex justify-center items-center">
                         <img width="120" height="30" class="inline md:w-32" src="/img/logos/react.png" />  
-                    </div>
-                    <div class="w-1/2 md:w-1/4 mb-4 flex justify-center items-center">
+                    </a>
+                    <a href="https://nova.laravel.com/docs/installation.html" class="w-1/2 md:w-1/4 mb-4 flex justify-center items-center">
                         <x-nova-icon class="w-[80%] md:w-44" />
-                    </div>
-                    <div class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
+                    </a>
+                    <a href="https://filamentphp.com/docs/3.x/panels/installation" class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
                         <x-filament-icon class="w-[70%] md:w-32" />
-                    </div>
-                    <div class="w-1/2 md:w-1/5 mb-4 flex justify-center text-xs items-center gap-1 font-bold">
+                    </a>
+                    <a href="https://spatie.be/docs/laravel-permission/v4/introduction" class="w-1/2 md:w-1/5 mb-4 flex justify-center text-xs items-center gap-1 font-bold">
                         <x-spatie-icon class="w-[70%] md:w-24" /> Permission
-                    </div>
-                    <div class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
+                    </a>
+                    <a href="https://pestphp.com/docs/installation" class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
                         <img width="120" height="30" class="inline md:w-32" src="/img/logos/pest.svg" />  
-                    </div>
-                    <div class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
+                    </a>
+                    <a href="https://carbon.nesbot.com/docs/" class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
                         <img width="120" height="30" class="inline md:w-30" src="/img/logos/carbon.png" />  
-                    </div>
-                    <div class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
+                    </a>
+                    <a href="https://nativephp.com/docs/1/getting-started/introduction" class="w-1/2 md:w-1/5 mb-4 flex justify-center items-center">
                         <img width="120" height="30" class="inline md:w-36" src="/img/logos/nativephp.svg" />  
-                    </div>
+                    </a>
                 </div>
             </div>
 
